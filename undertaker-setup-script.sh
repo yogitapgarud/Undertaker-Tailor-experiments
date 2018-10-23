e6001a9445cc52ec6ce8f308ea9c2dc0e60f2c54
Yogita <garudyogitap@gmail.com>
2018-10-23 12:33:42 -0700

    setup-script

---
+++ undertaker-setup-script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#git clone https://i4gerrit.informatik.uni-erlangen.de/undertaker
+sudo apt-get install flex bison libc6-dev libc6-dev-i386 linux-libc-dev linux-libc-dev:i386 libgmp3-dev libmpfr-dev libmpc-dev build-essential bc libboost1.55-dev libboost-filesystem1.55-dev libboost-regex1.55-dev libboost-thread1.55-dev libboost-wave1.55-dev libpuma-dev libpstreams-dev check python-unittest2 clang sparse pylint
