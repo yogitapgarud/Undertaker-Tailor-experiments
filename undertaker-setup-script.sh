d7f4499033cb5b5fcdd293e53e066c29043918ca
Yogita <yogita@yogita-VirtualBox.(none)>
2018-11-06 11:03:13 -0800

    for 12.04

--- undertaker-setup-script.sh
+++ undertaker-setup-script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt-get install --reinstall ca-certificates
+
 git clone https://i4gerrit.informatik.uni-erlangen.de/undertaker
 
 sudo apt-get install flex bison libc6-dev libc6-dev-i386 linux-libc-dev linux-libc-dev:i386 libgmp3-dev libmpfr-dev libmpc-dev build-essential bc libboost1.55-dev libboost-filesystem1.55-dev libboost-regex1.55-dev libboost-thread1.55-dev libboost-wave1.55-dev libpuma-dev libpstreams-dev check python-unittest2 clang sparse pylint
